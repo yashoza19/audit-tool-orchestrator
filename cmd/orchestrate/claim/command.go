31f40efe03ae8c393af9b0bff0f062cfea721544
Melvin Hillsman <mhillsma@redhat.com>
2022-03-14 03:29:59 -0500

    Add claim subcommand flags, flag validation/help, and minor refactoring.

--- cmd/orchestrate/claim/command.go
+++ cmd/orchestrate/claim/command.go
@@ -3,54 +3,106 @@ package claim
 // create and delete ClusterClaim resource
 
 import (
-	"audit-tool-orchestrator/pkg"
+	"audit-tool-orchestrator/pkg/orchestrate"
 	"context"
+	"github.com/google/uuid"
 	hivev1 "github.com/openshift/hive/apis/hive/v1"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strings"
 )
+
+var flags = orchestrate.ClaimFlags{}
 
 func NewCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "claim",
-		Short:   "",
-		Long:    "",
+		Use:   "claim",
+		Short: "Create a Hive ClusterClaim resource.",
+		Long: "Create a ClusterClaim resource to get a cluster from a ClusterPool. If a cluster is not available " +
+			"because all clusters have been claimed or none are available due to errors a cluster will be created in " +
+			"to fulfill this claim.",
 		PreRunE: validation,
 		RunE:    run,
 	}
+
+	cmd.Flags().StringVar(&flags.Name, "name", "ato-cluster-claim",
+		"Name for the ClusterClaim resource. Required when --delete is set.")
+	cmd.Flags().StringVar(&flags.Namespace, "namespace", "hive",
+		"OpenShift project (namespace) the ClusterClaim should be created in.")
+	cmd.Flags().StringVar(&flags.PoolName, "pool-name", "ato-cluster-pool",
+		"ClusterPool to claim cluster from. If cluster is not available one will be created; ~45 minutes to create.")
+	cmd.Flags().StringVar(&flags.BundleName, "bundle-name", "",
+		"name SQLite column value for bundle this ClusterClaim is for.")
+	cmd.Flags().BoolVar(&flags.Delete, "delete", false,
+		"Delete the ClusterClaim provided by the name flag. If you do not provide the name and set "+
+			"the --delete flag command will fail.")
 
 	return cmd
 }
 
 func validation(cmd *cobra.Command, args []string) error {
+	flags.Name = strings.TrimSpace(flags.Name)
+
+	if flags.Delete && (flags.Name == "ato-cluster-claim" || flags.Name == "") {
+		return &orchestrate.ClusterClaimDeleteFlagSetNameFlagEmptyError{}
+	}
+
+	if len(flags.Name) < 8 || len(flags.Name) > 16 {
+		return &orchestrate.ClusterClaimNameLengthIncorrectError{}
+	}
+
+	// Need to validate the name conforms to k8s naming convention
+	/*validateClusterClaimName := regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString
+	if err := validateClusterClaimName(flags.Name); err != false {
+		return &orchestrate.ClusterClaimNameHasInvalidCharactersError{}
+	}*/
+
 	return nil
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	// TODO: check if delete flag is set
-	// NOTE: there will be a claim per bundle run with name being set to the bundle.PackageName
-	// TODO: add a label that is the bundle.name
-	// TODO: ClusterClaim name needs unique ID; split UUID and add UUID[0]
 	ctx := context.Background()
+
+	hvclient := orchestrate.GetHiveClient()
+
+	uniqueId := uuid.New()
+	claimName := flags.Name + "-" + strings.Split(uniqueId.String(), "-")[0]
+
+	if flags.Delete {
+		claimName = flags.Name
+	}
 
 	cc := hivev1.ClusterClaim{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ato-cluster-claim",
-			Namespace: "hive",
+			Name:      claimName,
+			Namespace: flags.Namespace,
+			Labels:    map[string]string{"bundle-name": flags.BundleName},
 		},
 		Spec: hivev1.ClusterClaimSpec{
-			ClusterPoolName: "ato-cluster-pool",
+			ClusterPoolName: flags.PoolName,
 		},
 	}
 
-	hvclient := pkg.GetHiveClient()
-	if err := hvclient.Create(ctx, &cc); err != nil {
-		log.Errorf("Unable to create ClusterClaim: %v\n", err)
-		return err
+	if flags.Delete {
+		if err := hvclient.Delete(ctx, &cc); err != nil {
+			log.Errorf("Unable to delete ClusterClaim %s: %v\n", flags.Name, err)
+			return err
+		}
+
+		log.Infof("ClusterClaim %s deleted.\n", claimName)
+
+		return nil
+	} else {
+		if err := hvclient.Create(ctx, &cc); err != nil {
+			log.Errorf("Unable to create ClusterClaim %s: %v\n", claimName, err)
+			return err
+		}
+
+		log.Infof("ClusterClaim %s submitted. Waiting for Pending and ClusterRunning statuses", claimName)
 	}
 
-	// Delete ClusterClaim resource
-	// pass --delete and --claim
+	// ClusterClaim was created, we need to wait for Pending (False) and ClusterRunning (True) statuses
+
 	return nil
 }
